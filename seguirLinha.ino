b0454dab5273ea6bb04ae2a04a59400cba8cc406
Marcos Bah <marcos.ba@protonmail.com>
2019-04-27 16:16:58 -0300

    Adicionado comentários e update na sintaxe.

--- seguirLinha.ino
+++ seguirLinha.ino
@@ -1,51 +1,78 @@
-  int portaAnD = 0;
-  int portaDigD = 8;
-  int portaAnE = 1;
-  int portaDigE = 7;
+  //Programa em desenvolvimento
+
+  /*
+    IFSULDEMINAS - Campus Avançado Carmo de Minas
+    Criado por: Marcos Barbosa
+    Equipe: Marcos Barbosa, Pedro, Murilo Rocha, Julia Almeida.
+    Participação na elaboração do Projeto: Juliete Costa, Isabel Bento (Docentes) - Mariana Barcelar, Emannuel (Discentes)
+
+    Obs.:
+       #################|Wiki:|##################################################################
+       # Direita ou Esquerda simbolizam a direção (Norte ou Sul, sendo o norte o bico do robo). #
+       # D ou E significam respectivamente Direita e Esquerda.                                  #
+       ##########################################################################################
+
+       #################|Sintaxe:|###############################################################
+       # Toda as alterações devem ser ricas em comentarios e manterem a sintaxe e semantica, com#
+       # espaçcamentos e omissao de acentuacoes.                                                #
+       ##########################################################################################
+  */
+  
+  
+  //sensor reflexivo
+  int portaAnD = 0; //D
+  int portaDigD = 8; //D
+  int portaAnE = 1; //E
+  int portaDigE = 7; //E
 
   //motor
-  int motor_1_gira_esquerda = 11; //direcao
-  int motor_1_gira_direita = 10; //direcao
-  int motor_2_gira_esquerda = 12; //direcao
-  int motor_2_gira_direita = 13; //direcao
+  int motor_esquerda_1 = 11; //direcao
+  int motor_direita_1 = 10; 
+  int motor_esquerda_2 = 12; 
+  int motor_direita_2 = 13; 
   
+  //variaveis para armazenar valores dos sensores
   long val = 0;
   long val1 = 0;
 
+  //variavel para regular igualdade entre sensores
   int reg = 50;
+
+  //variavel para armazenar a diferença entre sensores
   int diff = 0;
 
 void setup() {
 
-  Serial.begin(9600);
-  pinMode(portaAnD, OUTPUT);
+  Serial.begin(9600); //inicializando Serial
+
+  pinMode(portaAnD, OUTPUT); //inicializando portas dos sensores
   pinMode(portaDigD, OUTPUT);
   pinMode(portaAnE, OUTPUT);
   pinMode(portaDigE, OUTPUT);
 
   //motor
-  pinMode(motor_1_gira_esquerda, OUTPUT);
-  pinMode(motor_1_gira_direita, OUTPUT);
-  pinMode(motor_2_gira_esquerda, OUTPUT);
-  pinMode(motor_2_gira_direita, OUTPUT);
+  pinMode(motor_esquerda_1, OUTPUT); //inicializando portas dos motores
+  pinMode(motor_direita_2, OUTPUT);
+  pinMode(motor_esquerda_1, OUTPUT);
+  pinMode(motor_direita_2, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  val = analogRead(portaAnD);
+
+  val = analogRead(portaAnD); //recebendo valores dos sensores analogicos
   val1 = analogRead(portaAnE);
 
-  if (val>val1){
+  if (val>val1){ //calculando o diferencial entre elas
     diff = val-val1;
   }else{
     diff = val1-val;
   }
 
-  if (val1 < val and diff > reg) {
+  if (val1 < val and diff > reg) { //verificando se sensor interpreta a linha, considerando erro de igualdade
       motor_1(1);
       stop2();
-      Serial.println("Lignado motor 1");
+      Serial.println("Ligando motor 1"); //escrevendo no serial caso os motores não estejam disponiveis
   }else{
     if(val < val1 and diff > reg){
        motor_2(1);
@@ -58,43 +85,43 @@ void loop() {
     }
   }
 
-  Serial.println(val);
+  Serial.println(val); //escrevendo os valores lidos nos sensores analogicos
   Serial.println(val1);
   delay(1000);
 }
-
-void motor_1(int direcao)
+//inicio das funcoes
+void motor_1(int direcao) //definindo direcao do motor 1
 {
   boolean inPin1 = LOW;
   boolean inPin2 = HIGH;
 
-  if(direcao == 1){
+  if(direcao == 1){ //caso seja 1, inverta o sentido
     inPin1 = HIGH;
     inPin2 = LOW;
   }
-    digitalWrite(motor_1_gira_esquerda, inPin1);
-    digitalWrite(motor_1_gira_direita, inPin2);
+    digitalWrite(motor_esquerda_1, inPin1);
+    digitalWrite(motor_direita_1, inPin2);
 }
 
-void stop1(){
-    digitalWrite(motor_1_gira_esquerda, LOW);
-    digitalWrite(motor_1_gira_direita, LOW);
+void stop1(){ //parando motor 1
+    digitalWrite(motor_esquerda_1, LOW);
+    digitalWrite(motor_direita_1, LOW);
 }
 
-void motor_2(int direcao)
+void motor_2(int direcao) //definindo direcao do motor 2
 {
   boolean inPin10 = LOW;
   boolean inPin20 = HIGH;
 
-  if(direcao == 1){
+  if(direcao == 1){ //caso seja 1, inverta o sentido
     inPin10 = HIGH;
     inPin20 = LOW;
   }
-    digitalWrite(motor_2_gira_esquerda, inPin10);
-    digitalWrite(motor_2_gira_direita, inPin20);
+    digitalWrite(motor_esquerda_2, inPin10);
+    digitalWrite(motor_direita_2, inPin20);
 }
 
-void stop2(){
-    digitalWrite(motor_2_gira_esquerda, LOW);
-    digitalWrite(motor_2_gira_direita, LOW);
+void stop2(){ //parando motor 2
+    digitalWrite(motor_esquerda_2, LOW);
+    digitalWrite(motor_direita_2, LOW);
 }
